4ad10859eeefb5beecf4181789a4bf67d54ea437
atiqmurshed <atiqmursherd631@gmail.com>
2024-09-20 10:54:49 -0400

    second commit

---
+++ js/login.js
@@ -0,0 +1,14 @@
+// console.log('click the button');
+document.getElementById('button-login')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        console.log('login button clicked');
+//step-3 get the phone number;
+        const phoneNumber = document.getElementById('phone-number').value;
+        // console.log(phoneNumber);
+
+
+
+})
+
+
